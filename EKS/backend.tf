65aae6d59aa57790669a9d38efb330de37a027e5
Antony Jebinraj Y <125467398+antonyjebin@users.noreply.github.com>
2024-05-31 15:14:32 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "cicd-terraform-eks"
+    bucket = "terraform-jenkins-eks-01"
     key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    region = "us-west-2"
   }
 }
