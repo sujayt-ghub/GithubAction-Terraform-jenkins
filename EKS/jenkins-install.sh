899e6671cc34010761d03c5053fa33c33148df81
sujayt-ghub <94687977+sujayt-ghub@users.noreply.github.com>
2025-02-28 17:04:05 +0530

    Update jenkins-install.sh

--- EKS/jenkins-install.sh
+++ EKS/jenkins-install.sh
@@ -7,9 +7,9 @@ sudo wget -O /etc/yum.repos.d/jenkins.repo \
    https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade
+sudo yum upgrade -y
 sudo yum install java -y
-sudo yum install jenkins.noarch
+sudo yum install jenkins.noarch -y
 sudo systemctl daemon-reload
 sudo systemctl enable jenkins.service
 sudo systemctl start jenkins
