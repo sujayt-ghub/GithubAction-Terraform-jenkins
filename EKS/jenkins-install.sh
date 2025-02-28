4c7a8ea4e205466577f585cc06e0fa29ef04aa87
sujayt-ghub <94687977+sujayt-ghub@users.noreply.github.com>
2025-02-28 14:32:23 +0530

    Update jenkins-install.sh

--- EKS/jenkins-install.sh
+++ EKS/jenkins-install.sh
@@ -5,11 +5,14 @@ sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+sudo yum upgrade
+# Add required dependencies for the jenkins package
+sudo yum install fontconfig java-17-openjdk
+sudo yum install jenkins
+sudo systemctl daemon-reload
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
+sudo systemctl status jenkins
 
 # then install git
 sudo yum install git -y
