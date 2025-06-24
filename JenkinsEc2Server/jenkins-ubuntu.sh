cf3ab472a2807ecbe6f286e84b5416b24650efa6
sujayt-ghub <94687977+sujayt-ghub@users.noreply.github.com>
2025-06-24 15:56:13 +0530

    Create jenkins-ubuntu.sh

---
+++ JenkinsEc2Server/jenkins-ubuntu.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# install jenkins
+sudo yum update -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo yum install java -y
+sudo yum install jenkins.noarch -y
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins.service
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+
+
+# then install git
+sudo yum install git -y
+
+#then install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+#finally install kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
