1f00f971da0dcc33f326062ec8fbf9a3ef77e19d
sujayt-ghub <94687977+sujayt-ghub@users.noreply.github.com>
2025-06-24 16:38:16 +0530

    Update jenkins-ubuntu.sh

--- JenkinsEc2Server/jenkins-ubuntu.sh
+++ JenkinsEc2Server/jenkins-ubuntu.sh
@@ -25,9 +25,9 @@ sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
 wget -O- https://apt.releases.hashicorp.com/gpg | \
  gpg --dearmor | \
  sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
- echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
- sudo apt update -y
- sudo apt-get install terraform -y
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update -y
+sudo apt-get install terraform -y
  
  
 
