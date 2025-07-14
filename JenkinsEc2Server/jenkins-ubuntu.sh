f26b2ff68a6454d3386ba9cfe0a564bbc6ecbd69
sujayt-ghub <94687977+sujayt-ghub@users.noreply.github.com>
2025-07-14 14:55:21 +0530

    Update jenkins-ubuntu.sh

--- JenkinsEc2Server/jenkins-ubuntu.sh
+++ JenkinsEc2Server/jenkins-ubuntu.sh
@@ -11,13 +11,13 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-#sudo apt-get update -y
-#sudo apt-get install jenkins -y
-##sudo systemctl enable jenkins
-#sudo systemctl enable jenkins.service
-#sudo systemctl daemon-reload
-#sudo systemctl start jenkins
-#sudo systemctl status jenkins
+sudo apt-get update -y
+sudo apt-get install jenkins -y
+#sudo systemctl enable jenkins
+sudo systemctl enable jenkins.service
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl status jenkins
 
 
 
@@ -28,8 +28,8 @@ sudo apt install git -y
 sudo apt install docker.io -y
 sudo chmod 666 /var/run/docker.sock
 
-mkdir jenkins
-docker run -d --name jenkins -p 8080:8080 -v $PWD/jenkins/ jenkins/jenkins:jdk17
+#mkdir jenkins
+#docker run -d --name jenkins -p 8080:8080 -v $PWD/jenkins/ jenkins/jenkins:jdk17
 
 #then install terraform
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
